3a5b6a44c74c943d008dc45bc027300b5ef8ec11
Anh Khoa <=khoanguyenanh218@gmail.com>
2025-05-31 20:32:07 +0700

    .

---
+++ fruit-scale-backend/src/main/java/com/pbl5/fruitscalebackend/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.pbl5.fruitscalebackend.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Fruit Scale Backend API")
+                        .version("1.0")
+                        .description("API documentation for Fruit Scale Backend"));
+    }
+}
